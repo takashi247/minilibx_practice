90a2d602290160dcf960596ab1a0caf27f6a904e
takashi247 <takashi247@gmail.com>
2021-07-31 18:11:31 +0900

    add program to draw triangle

---
+++ srcs/draw_triangle.c
@@ -0,0 +1,54 @@
+#include "mlx.h"
+#include "minilibx_practice.h"
+
+void
+	mlx_pixel_put_screen(t_screen *screen, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = screen->addr + (y * screen->line_length + x * (screen->bpp / 8));
+	*(unsigned int*)dst = color;
+}
+
+int
+	main(void)
+{
+	void		*mlx;
+	void		*mlx_win;
+	static int	width = 500;
+	static int	height = 500;
+	t_screen	screen;
+	int			col;
+	int			row;
+	const int	c_start = 100;
+	const int	c_end = 400;
+	const int	r_start = 100;
+	const int	r_end = 400;
+
+	mlx = mlx_init();
+	if (!mlx)
+		return (1);
+	mlx_win = mlx_new_window(mlx, width, height, "Hellow world!");
+	if (!mlx_win)
+		return (1);
+	screen.img = mlx_new_image(mlx, width, height);
+	if (!screen.img)
+		return (1);
+	screen.addr = mlx_get_data_addr(screen.img, &screen.bpp, &screen.line_length, &screen.endian);
+	col = c_start;
+	while (col < c_end)
+	{
+		row = r_start;
+		while (row < r_end)
+		{
+			if (row == -2 * col + 600 || row == 2 * col - 400)
+				mlx_pixel_put_screen(&screen, col, row, 0x00FF0000);
+			else if (row == r_end - 1)
+				mlx_pixel_put_screen(&screen, col, row, 0x00FF0000);
+			row++;
+		}
+		col++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, screen.img, 0, 0);
+	mlx_loop(mlx);
+}
